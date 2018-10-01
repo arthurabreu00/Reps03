ef073f45bd389bc4409c40826f0d66295a947827
arthurabreu00 <arthurabreu00@gmail.com>
2018-10-01 13:16:32 -0300

    LocalStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
 
     } // Fechando o Constructor
     onEdit() {
@@ -102,6 +103,8 @@ class UserController {
             this.getPhoto(this.formEl).then(
                 (content) => {
                     values.photo = content;
+
+                    this.insert(values);
 
                     this.addLine(values);
 
@@ -214,9 +217,50 @@ class UserController {
         );
     } // Fechando o Método getValues();
 
+
+    getUsersStorage(){
+
+        let users = [];
+
+        if(sessionStorage.getItem("users")){
+
+            users = JSON.parse(sessionStorage.getItem("users"))
+
+        }
+
+        return users;
+    }
+
+    selectAll(){
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser =>{
+            
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+
+        })
+
+
+    }
+
+    insert(data){
+
+        let users = this.getUsersStorage();
+        users.push(data);
+        sessionStorage.setItem("users",JSON.stringify(users))
+
+    }
+
     addLine(dataUser) {
 
         let tr = document.createElement("tr");
+        
+    
 
         tr.dataset.user = JSON.stringify(dataUser);
 
@@ -249,6 +293,7 @@ class UserController {
 
             if(confirm("Deseja realmente excluir ?")){
                 tr.remove();
+                this.updateCount();
             }
 
         });
